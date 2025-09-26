2a2863c5628b3a5057aaf67952f191f1078f1943
mariam-al0 <166653306+mariam-al0@users.noreply.github.com>
2025-09-26 15:06:37 -0400

    Pushing KittyTest examples

--- src/test/KittyTest.java
+++ src/test/KittyTest.java
@@ -1,7 +1,12 @@
 package test;
-import org.junit.Test;
-import activity.Kitty;
 
+import activity.Kitty;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 public class KittyTest {
 
@@ -11,23 +16,46 @@ public class KittyTest {
      contents, we can use a testing utility! Because this is a normal method, we do not need to use
      the @Test annotation. **/
 
-    public void compareKitties(){
+    public void compareKitties(Kitty Tom, Kitty Pepper){
         //place testing utility here
+        assertEquals(Tom.getName(), Pepper.getName());
+        assertEquals(Tom.getAge(), Pepper.getAge());
+        assertEquals(Tom.getWeight(), Pepper.getWeight(), .001);
     }
 
     /** Write tests for your constructor. **/
     @Test
     public void constructorTest(){
         //place tests here
+        Kitty Tom = new Kitty("Tom", 5, 20.0);
+        assertEquals("Tom", Tom.getName());
+        assertEquals(5, Tom.getAge());
+        assertEquals(20.0, Tom.getWeight(), .001);
     }
 
     @Test
     public void averageWeightTest(){
         //place tests here
+        Kitty Tom = new Kitty("Tom", 5, 20.0);
+        Kitty Pepper = new Kitty("Pepper", 2, 10.0);
+        assertEquals(15.0, Tom.averageWeight(Pepper), .001);
     }
 
     @Test
     public void kittenSorterTest(){
         //place test here
+        Kitty Tom = new Kitty("Tom", 5, 20.0);
+        Kitty Pepper = new Kitty("Pepper", 2, 10.0);
+        ArrayList<Kitty> kittenList = new ArrayList<>(List.of(Tom, Pepper));
+        ArrayList<Kitty> actualList = Tom.kittenSorter(kittenList);
+        ArrayList<Kitty> expectedList = new ArrayList<>(List.of(Pepper));
+
+        assertEquals(actualList.size(),expectedList.size());
+
+        for(int i = 0; i < actualList.size(); i++){
+            compareKitties(expectedList.get(i), actualList.get(i));
+        }
     }
 }
+
+
